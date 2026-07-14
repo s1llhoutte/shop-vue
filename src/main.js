6a9f03f96cc73670adc66185c6db5e7e42c130ae
s1llhoutte <danikval2000002@gmail.com>
2026-07-14 18:10:18 +0300

    add connection with admin for get products

--- src/main.js
+++ src/main.js
@@ -1,7 +1,10 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import axios from 'axios'
 
-createApp(App)
-    .use(router)
-    .mount('#app')
+const app = createApp(App)
+
+app.use(router)
+app.config.globalProperties.$axios = axios
+app.mount('#app')
